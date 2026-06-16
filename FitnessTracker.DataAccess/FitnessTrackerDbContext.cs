c440b85811da19ef0e68c8403025bb44e9c1fe60
Рома Авраменко <www.kioma220@gmail.com>
2026-06-16 17:48:58 +0300

    feat: add db context

---
+++ FitnessTracker.DataAccess/FitnessTrackerDbContext.cs
@@ -0,0 +1,19 @@
+﻿using FitnessTracker.DataAccess.Configuration;
+
+namespace FitnessTracker.DataAccess
+{
+    public class FitnessTrackerDbContext(DbContextOptions<FitnessTrackerDbContext> options)
+        : DbContext(options)
+    {
+        public DbSet<User> Users {  get; set; }
+        public DbSet<Workout> Workouts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new UsersConfiguration());
+            modelBuilder.ApplyConfiguration(new WorkoutsConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
