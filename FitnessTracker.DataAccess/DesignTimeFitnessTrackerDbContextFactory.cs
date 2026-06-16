df3cd990af4aaad6af11fddc90ba1da08cbe88c8
Рома Авраменко <www.kioma220@gmail.com>
2026-06-16 18:40:16 +0300

    feat: add design time db context factory

---
+++ FitnessTracker.DataAccess/DesignTimeFitnessTrackerDbContextFactory.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+
+namespace FitnessTracker.DataAccess
+{
+    public class DesignTimeFitnessTrackerDbContextFactory
+        : IDesignTimeDbContextFactory<FitnessTrackerDbContext>
+    {
+        public FitnessTrackerDbContext CreateDbContext(string[] args)
+        {
+            DotNetEnv.Env.TraversePath().Load("designtime.env");
+
+            var host = Environment.GetEnvironmentVariable("DB_HOST");
+            var port = Environment.GetEnvironmentVariable("DB_PORT");
+            var db = Environment.GetEnvironmentVariable("DB_NAME");
+            var user = Environment.GetEnvironmentVariable("DB_USER");
+            var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+            if (host is null ||
+                port is null ||
+                db is null ||
+                user is null ||
+                password is null)
+            {
+                throw new EnviormnetVariableNotFound("Can't Find Enviorment Variables for Connection String");
+            }
+
+            var connectionString =
+                    $"Host={host};Port={port};Database={db};Username={user};Password={password}";
+
+            var optionsBuilder = new DbContextOptionsBuilder<FitnessTrackerDbContext>()
+                .UseNpgsql(connectionString);
+
+            return new FitnessTrackerDbContext(optionsBuilder.Options);
+        }
+    }
+}
---
+++ FitnessTracker.Shared/Exceptions/EnviormnetVariableNotFound.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessTracker.Shared.Exceptions
+{
+    public class EnviormnetVariableNotFound 
+        : Exception
+    {
+        public EnviormnetVariableNotFound() 
+            :base()
+        { }
+
+        public EnviormnetVariableNotFound(string message)
+            : base(message)
+        { }
+    }
+}
