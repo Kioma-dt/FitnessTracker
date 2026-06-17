5f25b3307214065ba5b730bac814dd3980d691d1
Рома Авраменко <www.kioma220@gmail.com>
2026-06-17 16:23:20 +0300

    fix!: change login exceptions catch

--- FitnessTracker.API/Controllers/UserController.cs
+++ FitnessTracker.API/Controllers/UserController.cs
@@ -39,15 +39,9 @@ namespace FitnessTracker.API.Controllers
         }
 
         [HttpPost("login")]
-        public async Task<
-            Results<Ok<SuccessResponse<LoginResponseDTO>>,
-                BadRequest<StatusResponse>,
-                UnauthorizedHttpResult,
-                InternalServerError<StatusResponse>>
-            > Login([FromBody] LoginRequestDTO request)
+        public async Task<Ok<LoginResponseDTO>>
+            Login([FromBody] LoginRequestDTO request)
         {
-            try
-            {
                 var user = await _usersRepository.GetByNameAsync(request.UserName);
 
                 if (user is null)
@@ -65,18 +59,7 @@ namespace FitnessTracker.API.Controllers
 
                 var jwtToken = _jwtTokenFactory.Create(user);
 
-                return TypedResults.Ok(new SuccessResponse<LoginResponseDTO>(200,
-                    "Loged In Successfully",
-                    new LoginResponseDTO(jwtToken, user.Id ?? String.Empty, request.UserName)));
-            }
-            catch(LoginException ex)
-            {
-                return TypedResults.Unauthorized();
-            }
-            catch(Exception ex)
-            {
-                return TypedResults.InternalServerError(new StatusResponse(500, ex.Message));
-            }
+                return TypedResults.Ok(new LoginResponseDTO(jwtToken, user.Id ?? String.Empty, request.UserName));
         }
     }
 }
