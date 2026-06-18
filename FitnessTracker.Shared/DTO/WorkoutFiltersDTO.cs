c76c4558d8b00f99fe553d92b8d9e2f87c640849
Рома Авраменко <www.kioma220@gmail.com>
2026-06-18 16:02:44 +0300

    feat: add workout filters dto

---
+++ FitnessTracker.Shared/DTO/WorkoutFiltersDTO.cs
@@ -0,0 +1,16 @@
+﻿using FitnessTracker.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessTracker.Shared.DTO
+{
+    public record WorkoutFiltersDTO
+    (
+        DateTime? FromDate,
+        DateTime? ToDate,
+        TimeSpan? MinDuration,
+        TimeSpan? MaxDuration,
+        WorkoutType? Type
+    );
+}
