8206a50ff378d14cb8fc22ab9d14a86dde8d05e7
Рома Авраменко <www.kioma220@gmail.com>
2026-06-18 16:51:31 +0300

    feat: add workout expression builder

---
+++ FitnessTracker.Application/WorkoutFilters/WorkoutFilterExpressionBuilder.cs
@@ -0,0 +1,44 @@
+﻿using FitnessTracker.Shared.DTO;
+using FitnessTracker.Shared.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace FitnessTracker.Application.WorkoutFilters
+{
+    public interface IWorkoutFilterExpressionBuilder
+    {
+        Expression<Func<Workout, bool>> BuildFilterExpression(IEnumerable<WorkoutFilterDTO> filters); 
+    }
+
+    public class WorkoutFilterExpressionBuilder 
+        : IWorkoutFilterExpressionBuilder
+    {
+        private readonly Dictionary<WorkoutFilterType, IWorkoutFilter> _filters;
+        public WorkoutFilterExpressionBuilder(IEnumerable<IWorkoutFilter> filters)
+        {
+            _filters = filters.ToDictionary(x => x.FilterType);
+        }
+        public Expression<Func<Workout, bool>> BuildFilterExpression(IEnumerable<WorkoutFilterDTO> filters)
+        {
+            var parameter = Expression.Parameter(typeof(Workout), "workout");
+            Expression? combinedExpression = null;
+            foreach (var filter in filters)
+            {
+                if(!_filters.TryGetValue(filter.FilterType, out var filterImplementation))
+                {
+                    throw new NotImplementedFunctionalityException($"No filter implementation found for filter type {filter.FilterType}");
+                }
+
+                var filterExpression = filterImplementation.BuildExpression(parameter, filter.FilterValue ?? string.Empty);
+                combinedExpression = combinedExpression == null ? filterExpression : Expression.AndAlso(combinedExpression, filterExpression);
+            }
+            if (combinedExpression == null)
+            {
+                combinedExpression = Expression.Constant(true);
+            }
+            return Expression.Lambda<Func<Workout, bool>>(combinedExpression, parameter);
+        }
+    }
+}
