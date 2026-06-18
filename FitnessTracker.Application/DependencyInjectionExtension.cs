5ad93a370ba07469033ccb6abebf0f6e21c4303c
Рома Авраменко <www.kioma220@gmail.com>
2026-06-18 16:53:31 +0300

    feat: add filters to DI

--- FitnessTracker.Application/DependencyInjectionExtension.cs
+++ FitnessTracker.Application/DependencyInjectionExtension.cs
@@ -2,6 +2,7 @@
 using FitnessTracker.Application.PasswordHasher;
 using FitnessTracker.Application.PhotosRemoteStorage;
 using FitnessTracker.Application.StreamImageChecker;
+using FitnessTracker.Application.WorkoutFilters;
 using Microsoft.Extensions.DependencyInjection;
 namespace FitnessTracker.Application
 {
@@ -11,9 +12,20 @@ namespace FitnessTracker.Application
             IServiceCollection services)
         {
             services.AddScoped<IPasswordHasher, BCryptPasswordHasher>();
+
             services.AddScoped<IJwtTokenFactory, IdentityJwtTokenFactory>();
+
             services.AddScoped<IPhotosRemoteStorage, ImageKitRemoteStorage>();
+
             services.AddScoped<IStreamImageChecker, SkiaSharpStreamImageChecker>();
+
+            services.AddScoped<IWorkoutFilter, FromDateWorkoutFilter>();
+            services.AddScoped<IWorkoutFilter, ToDateWorkoutFilter>();
+            services.AddScoped<IWorkoutFilter, MinDurationWorkoutFilter>();
+            services.AddScoped<IWorkoutFilter, MaxDurationWorkoutFilter>();
+            services.AddScoped<IWorkoutFilter, TypeWorkoutFilter>();
+
+            services.AddScoped<IWorkoutFilterExpressionBuilder, WorkoutFilterExpressionBuilder>();
             return services;
         }
     }
