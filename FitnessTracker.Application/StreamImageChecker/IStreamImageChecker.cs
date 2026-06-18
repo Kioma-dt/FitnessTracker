e6d75b83348d0a661339bcc538e24a12a833c7bf
Рома Авраменко <www.kioma220@gmail.com>
2026-06-18 13:36:32 +0300

    feat: add stream image checker

---
+++ FitnessTracker.Application/StreamImageChecker/IStreamImageChecker.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessTracker.Application.StreamImageChecker
+{
+    public interface IStreamImageChecker
+    {
+        Task<bool> IsSteamImage(Stream stream);
+    }
+}
