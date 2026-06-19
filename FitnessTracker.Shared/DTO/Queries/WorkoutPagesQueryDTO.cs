6574440da0c0f57f8cf16864fbe9034b4be6ba46
Рома Авраменко <www.kioma220@gmail.com>
2026-06-19 22:59:52 +0300

    feat: add pagination dtos

---
+++ FitnessTracker.Shared/DTO/Queries/WorkoutPagesQueryDTO.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessTracker.Shared.DTO.Queries
+{
+    public record WorkoutPagesQueryDTO
+    (
+        int Page = 1,
+        int PageSize = 10
+    );
+}
---
+++ FitnessTracker.Shared/DTO/Responses/PagedResponseDTO.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FitnessTracker.Shared.DTO.Responses
+{
+    public record PagedResponseDTO<T>
+    {
+        public int PageNumber { get; init; }
+        public int PageSize { get; init; }
+        public int TotalRecords { get; init; }
+        public int TotalPages { get; init; }
+        public List<T> Items { get; init; }
+
+        public PagedResponseDTO(List<T> items, int pageNumber, int pageSize, int totalRecords)
+        {
+            Items = items;
+            PageNumber = pageNumber;
+            PageSize = pageSize;
+            TotalRecords = totalRecords;
+            TotalPages = (int)Math.Ceiling((decimal)totalRecords / (decimal)pageSize);
+        }
+    }
+}
