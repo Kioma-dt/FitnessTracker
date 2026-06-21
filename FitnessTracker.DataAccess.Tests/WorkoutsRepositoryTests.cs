34d100c4634f28dfb125977b032800ac4f801b76
Рома Авраменко <www.kioma220@gmail.com>
2026-06-21 17:11:23 +0300

    test: add exercise tests

--- FitnessTracker.DataAccess.Tests/WorkoutsRepositoryTests.cs
+++ FitnessTracker.DataAccess.Tests/WorkoutsRepositoryTests.cs
@@ -162,6 +162,112 @@ namespace FitnessTracker.DataAccess.Tests
             Assert.Equal("user1", dbWorkout.UserId);
         }
 
+        [Fact]
+        public async Task AddExerciseAsync_ShouldAddExercise_WhenWorkoutExists()
+        {
+            using var context = CreateDbContext();
+
+            var repository = new WorkoutsRepository(context, _orderingApplierMock.Object, _filterBuilderMock.Object);
+
+            var date = DateTime.UtcNow;
+            var workout = new Workout
+            {
+                Id = "1",
+                UserId = "user1",
+                Title = "Leg day",
+                Type = WorkoutType.Strength,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = date,
+                Exercises =
+                [
+                    new Exercise
+                    {
+                        Name = "Squat",
+                        Sets =
+                        [
+                            new Set(10, 100),
+                            new Set(8, 120)
+                        ]
+                    }
+                ],
+                ProgressPhotos = ["photo1.jpg"]
+            };
+
+            context.Workouts.Add(workout);
+            await context.SaveChangesAsync();
+
+            var exercise = new Exercise
+            {
+                Name = "Lunges",
+                Sets =
+                [
+                    new Set(12, 50),
+                    new Set(10, 60)
+                ]
+            };
+
+            await repository.AddExerciseAsync("1", exercise);
+
+            var dbWorkout = await context.Workouts
+                .FirstOrDefaultAsync(x => x.Id == "1");
+
+            Assert.NotNull(dbWorkout);
+            Assert.Equal(2, dbWorkout.Exercises.Count);
+            var exercisesdb = dbWorkout.Exercises.OrderBy(x => x.Name).ToList();
+            Assert.Equal("Lunges", exercisesdb[0].Name);
+            Assert.Equal("Squat", exercisesdb[1].Name);
+        }
+
+        [Fact]
+        public async Task AddExerciseAsync_ShouldThrow_WhenWorkoutNotExists()
+        {
+            using var context = CreateDbContext();
+
+            var repository = new WorkoutsRepository(context, _orderingApplierMock.Object, _filterBuilderMock.Object);
+
+            var date = DateTime.UtcNow;
+            var workout = new Workout
+            {
+                Id = "1",
+                UserId = "user1",
+                Title = "Leg day",
+                Type = WorkoutType.Strength,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = date,
+                Exercises =
+                [
+                    new Exercise
+                    {
+                        Name = "Squat",
+                        Sets =
+                        [
+                            new Set(10, 100),
+                            new Set(8, 120)
+                        ]
+                    }
+                ],
+                ProgressPhotos = ["photo1.jpg"]
+            };
+
+            context.Workouts.Add(workout);
+            await context.SaveChangesAsync();
+
+            var exercise = new Exercise
+            {
+                Name = "Lunges",
+                Sets =
+                [
+                    new Set(12, 50),
+                    new Set(10, 60)
+                ]
+            };
+
+            await Assert.ThrowsAsync<EntityNotFoundException>(() => repository.AddExerciseAsync("2", exercise));
+        }
+
+
         private FitnessTrackerDbContext CreateDbContext()
         {
             var options = new DbContextOptionsBuilder<FitnessTrackerDbContext>()
