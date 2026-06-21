baefbb7b647653c062911860e104deec33e37fa5
Рома Авраменко <www.kioma220@gmail.com>
2026-06-21 20:10:48 +0300

    test: workout filters tests

---
+++ FitnessTracker.Application.Tests/WorkoutFiltersTests/WorkoutFilterExpressionBuilderTests.cs
@@ -0,0 +1,210 @@
+﻿using FitnessTracker.Application.WorkoutFilters;
+using FitnessTracker.Entities;
+using FitnessTracker.Shared.DTO;
+using FitnessTracker.Shared.Enums;
+using FitnessTracker.Shared.Exceptions;
+using Moq;
+using System.Linq.Expressions;
+
+namespace FitnessTracker.Application.Tests.WorkoutFiltersTests
+{
+    public class WorkoutFilterExpressionBuilderTests
+    {
+        [Fact]
+        public void BuildFilterExpression_ShouldReturnTrue_WhenNoFiltersProvided()
+        {
+            var filtersImplementaions = new List<IWorkoutFilter>();
+
+            var filters = new List<WorkoutFilterDTO>();
+
+            var workout = new Workout();
+
+            var builder = new WorkoutFilterExpressionBuilder(filtersImplementaions);
+
+            var expression = builder.BuildFilterExpression(filters);
+
+            var compiledExpression = expression.Compile();
+
+            Assert.True(compiledExpression(workout));
+        }
+
+
+        [Fact]
+        public void BuildFilterExpression_ShouldCallFilterImplementation_WhenFilterExists()
+        {
+            var filterMock = new Mock<IWorkoutFilter>();
+            filterMock
+                .Setup(x => x.FilterType)
+                .Returns(WorkoutFilterType.WorkoutType);
+            filterMock
+                .Setup(x => x.BuildExpression(
+                    It.IsAny<ParameterExpression>(),
+                    "Cardio"))
+                .Returns(Expression.Constant(true));
+
+            var filtersImplementaions = new List<IWorkoutFilter>() { filterMock.Object };
+
+            var filters = new List<WorkoutFilterDTO>
+            {
+                new(
+                    WorkoutFilterType.WorkoutType,
+                    "Cardio")
+            };
+
+            var workout = new Workout { Type = WorkoutType.Cardio };
+
+            var builder = new WorkoutFilterExpressionBuilder(filtersImplementaions);
+
+            var expression = builder.BuildFilterExpression(filters);
+
+            var compiledExpression = expression.Compile();
+
+            Assert.True(compiledExpression(workout));
+            filterMock.Verify(
+                x => x.BuildExpression(
+                    It.IsAny<ParameterExpression>(),
+                    "Cardio"),
+                Times.Once);
+        }
+
+
+        [Fact]
+        public void BuildFilterExpression_ShouldRetrunTrue_WhenBothFiltersTrue()
+        {
+            var filter1 = new Mock<IWorkoutFilter>();
+            filter1
+                .Setup(x => x.FilterType)
+                .Returns(WorkoutFilterType.MinDuration);
+            filter1
+                .Setup(x => x.BuildExpression(
+                    It.IsAny<ParameterExpression>(),
+                    It.IsAny<string>()))
+                .Returns(Expression.Constant(true));
+
+            var filter2 = new Mock<IWorkoutFilter>();
+            filter2
+                .Setup(x => x.FilterType)
+                .Returns(WorkoutFilterType.MaxDuration);
+            filter2
+                .Setup(x => x.BuildExpression(
+                    It.IsAny<ParameterExpression>(),
+                    It.IsAny<string>()))
+                .Returns(Expression.Constant(true));
+            var filtersImplementaions = new List<IWorkoutFilter>() { filter1.Object, filter2.Object };
+
+            var filters = new List<WorkoutFilterDTO>
+            {
+                new(
+                    WorkoutFilterType.MinDuration,
+                    "10"),
+
+                new(
+                    WorkoutFilterType.MaxDuration,
+                    "20")
+            };
+
+
+            var workout = new Workout { Duration = new TimeSpan(15) };
+
+            var builder = new WorkoutFilterExpressionBuilder(filtersImplementaions);
+
+            var expression = builder.BuildFilterExpression(filters);
+
+            var compiledExpression = expression.Compile();
+
+            Assert.True(compiledExpression(workout));
+            filter1.Verify(
+                x => x.BuildExpression(
+                    It.IsAny<ParameterExpression>(),
+                    "10"),
+                Times.Once);
+            filter2.Verify(
+                x => x.BuildExpression(
+                    It.IsAny<ParameterExpression>(),
+                    "20"),
+                Times.Once);
+        }
+
+        [Fact]
+        public void BuildFilterExpression_ShouldRetrunFalse_WhenOneFilterIsFalse()
+        {
+            var filter1 = new Mock<IWorkoutFilter>();
+            filter1
+                .Setup(x => x.FilterType)
+                .Returns(WorkoutFilterType.MinDuration);
+            filter1
+                .Setup(x => x.BuildExpression(
+                    It.IsAny<ParameterExpression>(),
+                    It.IsAny<string>()))
+                .Returns(Expression.Constant(true));
+
+            var filter2 = new Mock<IWorkoutFilter>();
+            filter2
+                .Setup(x => x.FilterType)
+                .Returns(WorkoutFilterType.MaxDuration);
+            filter2
+                .Setup(x => x.BuildExpression(
+                    It.IsAny<ParameterExpression>(),
+                    It.IsAny<string>()))
+                .Returns(Expression.Constant(false));
+            var filtersImplementaions = new List<IWorkoutFilter>() { filter1.Object, filter2.Object };
+
+            var filters = new List<WorkoutFilterDTO>
+            {
+                new(
+                    WorkoutFilterType.MinDuration,
+                    "10"),
+
+                new(
+                    WorkoutFilterType.MaxDuration,
+                    "20")
+            };
+
+
+            var workout = new Workout { Duration = new TimeSpan(40) };
+
+            var builder = new WorkoutFilterExpressionBuilder(filtersImplementaions);
+
+            var expression = builder.BuildFilterExpression(filters);
+
+            var compiledExpression = expression.Compile();
+
+            Assert.False(compiledExpression(workout));
+            filter1.Verify(
+                x => x.BuildExpression(
+                    It.IsAny<ParameterExpression>(),
+                    "10"),
+                Times.Once);
+            filter2.Verify(
+                x => x.BuildExpression(
+                    It.IsAny<ParameterExpression>(),
+                    "20"),
+                Times.Once);
+        }
+
+
+        [Fact]
+        public void BuildFilterExpression_ShouldThrow_WhenFilterImplementationDoesNotExist()
+        {
+            var filter = new Mock<IWorkoutFilter>();
+            filter
+                .Setup(x => x.FilterType)
+                .Returns(WorkoutFilterType.MinDuration);
+            var filtersImplementaions = new List<IWorkoutFilter>() { filter.Object };
+
+            var filters = new List<WorkoutFilterDTO>
+            {
+                new(
+                    WorkoutFilterType.MaxDuration,
+                    "20")
+            };
+
+            var workout = new Workout { Duration = new TimeSpan(40) };
+
+            var builder = new WorkoutFilterExpressionBuilder(filtersImplementaions);
+
+            Assert.Throws<NotImplementedFunctionalityException>(
+                () => builder.BuildFilterExpression(filters));
+        }
+    }
+}
---
+++ FitnessTracker.Application.Tests/WorkoutFiltersTests/WorkoutFlitersTests.cs
@@ -0,0 +1,229 @@
+﻿using FitnessTracker.Application.WorkoutFilters;
+using FitnessTracker.Entities;
+using FitnessTracker.Shared.Enums;
+using FitnessTracker.Shared.Exceptions;
+using System.Linq.Expressions;
+using System.Reflection.Metadata;
+
+namespace FitnessTracker.Application.Tests.WorkoutFiltersTests
+{
+    public class WorkoutFlitersTests
+    {
+        [Fact]
+        public void FromDateBuildExpression_ShouldReturnTrue_WhenWorkoutDateIsAfterFromDate()
+        {
+            var fromDate = new DateTime(2026, 05, 21);
+
+            var workout = new Workout { WorkoutDate = new DateTime(2026, 06, 21) };
+
+            var filter = new FromDateWorkoutFilter();
+
+            var parameter = Expression.Parameter(typeof(Workout));
+            var expression = filter.BuildExpression(parameter, fromDate.ToString());
+
+            var compiledExpression = CompileExpression(parameter, expression);
+            Assert.True(compiledExpression(workout));
+        }
+
+        [Fact]
+        public void FromDateBuildExpression_ShouldReturnFalse_WhenWorkoutDateIsBeforeFromDate()
+        {
+            var fromDate = new DateTime(2026, 07, 21);
+
+            var workout = new Workout { WorkoutDate = new DateTime(2026, 06, 21) };
+
+            var filter = new FromDateWorkoutFilter();
+
+            var parameter = Expression.Parameter(typeof(Workout));
+            var expression = filter.BuildExpression(parameter, fromDate.ToString());
+
+            var compiledExpression = CompileExpression(parameter, expression);
+            Assert.False(compiledExpression(workout));
+        }
+
+        [Fact]
+        public void FromDateBuildExpression_ShouldThrow_WhenValueIsInvalid()
+        {
+            var filter = new FromDateWorkoutFilter();
+            Assert.Throws<WrongFilterValueFormatException>(() =>
+                filter.BuildExpression(
+                    Expression.Parameter(typeof(Workout)),
+                    "abc"));
+        }
+
+        [Fact]
+        public void ToDateBuildExpression_ShouldReturnTrue_WhenWorkoutDateIsBeforeToDate()
+        {
+            var toDate = new DateTime(2026, 07, 21);
+
+            var workout = new Workout { WorkoutDate = new DateTime(2026, 06, 21) };
+
+            var filter = new ToDateWorkoutFilter();
+
+            var parameter = Expression.Parameter(typeof(Workout));
+            var expression = filter.BuildExpression(parameter, toDate.ToString());
+
+            var compiledExpression = CompileExpression(parameter, expression);
+            Assert.True(compiledExpression(workout));
+        }
+
+        [Fact]
+        public void ToDateBuildExpression_ShouldReturnFalse_WhenWorkoutDateIsAfterToDate()
+        {
+            var toDate = new DateTime(2026, 05, 21);
+
+            var workout = new Workout { WorkoutDate = new DateTime(2026, 06, 21) };
+
+            var filter = new ToDateWorkoutFilter();
+
+            var parameter = Expression.Parameter(typeof(Workout));
+            var expression = filter.BuildExpression(parameter, toDate.ToString());
+
+            var compiledExpression = CompileExpression(parameter, expression);
+            Assert.False(compiledExpression(workout));
+        }
+
+        [Fact]
+        public void ToDateBuildExpression_ShouldThrow_WhenValueIsInvalid()
+        {
+            var filter = new ToDateWorkoutFilter();
+            Assert.Throws<WrongFilterValueFormatException>(() =>
+                filter.BuildExpression(
+                    Expression.Parameter(typeof(Workout)),
+                    "abc"));
+        }
+
+        [Fact]
+        public void MinDurationBuildExpression_ShouldReturnTrue_WhenWorkoutDurationIsGreaterMinDuration()
+        {
+            var minDuration = new TimeSpan(67);
+
+            var workout = new Workout { Duration = new TimeSpan(70) };
+
+            var filter = new MinDurationWorkoutFilter();
+
+            var parameter = Expression.Parameter(typeof(Workout));
+            var expression = filter.BuildExpression(parameter, minDuration.ToString());
+
+            var compiledExpression = CompileExpression(parameter, expression);
+            Assert.True(compiledExpression(workout));
+        }
+
+        [Fact]
+        public void MinDurationBuildExpression_ShouldReturnFalse_WhenWorkoutDurationIsLessMinDuration()
+        {
+            var minDuration = new TimeSpan(67);
+
+            var workout = new Workout { Duration = new TimeSpan(65) };
+
+            var filter = new MinDurationWorkoutFilter();
+
+            var parameter = Expression.Parameter(typeof(Workout));
+            var expression = filter.BuildExpression(parameter, minDuration.ToString());
+
+            var compiledExpression = CompileExpression(parameter, expression);
+            Assert.False(compiledExpression(workout));
+        }
+
+        [Fact]
+        public void MinDurationBuildExpression_ShouldThrow_WhenValueIsInvalid()
+        {
+            var filter = new MinDurationWorkoutFilter();
+            Assert.Throws<WrongFilterValueFormatException>(() =>
+                filter.BuildExpression(
+                    Expression.Parameter(typeof(Workout)),
+                    "abc"));
+        }
+
+        [Fact]
+        public void MaxDurationBuildExpression_ShouldReturnTrue_WhenWorkoutDurationIsLessMaxDuration()
+        {
+            var maxDuration = new TimeSpan(67);
+
+            var workout = new Workout { Duration = new TimeSpan(65) };
+
+            var filter = new MaxDurationWorkoutFilter();
+
+            var parameter = Expression.Parameter(typeof(Workout));
+            var expression = filter.BuildExpression(parameter, maxDuration.ToString());
+
+            var compiledExpression = CompileExpression(parameter, expression);
+            Assert.True(compiledExpression(workout));
+        }
+
+        [Fact]
+        public void MaxDurationBuildExpression_ShouldReturnFalse_WhenWorkoutDurationIsGreaterMaxDuration()
+        {
+            var maxDuration = new TimeSpan(67);
+
+            var workout = new Workout { Duration = new TimeSpan(70) };
+
+            var filter = new MaxDurationWorkoutFilter();
+
+            var parameter = Expression.Parameter(typeof(Workout));
+            var expression = filter.BuildExpression(parameter, maxDuration.ToString());
+
+            var compiledExpression = CompileExpression(parameter, expression);
+            Assert.False(compiledExpression(workout));
+        }
+
+        [Fact]
+        public void MaxDurationBuildExpression_ShouldThrow_WhenValueIsInvalid()
+        {
+            var filter = new MaxDurationWorkoutFilter();
+            Assert.Throws<WrongFilterValueFormatException>(() =>
+                filter.BuildExpression(
+                    Expression.Parameter(typeof(Workout)),
+                    "abc"));
+        }
+
+        [Fact]
+        public void WorkoutTypeBuildExpression_ShouldReturnTrue_WhenWorkoutTypeMatches()
+        {
+            WorkoutType type = WorkoutType.Strength;
+
+            var workout = new Workout { Type = WorkoutType.Strength };
+
+            var filter = new TypeWorkoutFilter();
+
+            var parameter = Expression.Parameter(typeof(Workout));
+            var expression = filter.BuildExpression(parameter, type.ToString());
+
+            var compiledExpression = CompileExpression(parameter, expression);
+            Assert.True(compiledExpression(workout));
+        }
+
+        [Fact]
+        public void WorkoutTypeBuildExpression_ShouldReturnFalse_WhenWorkoutTypeNotMatches()
+        {
+            WorkoutType type = WorkoutType.Strength;
+
+            var workout = new Workout { Type = WorkoutType.Cardio };
+
+            var filter = new TypeWorkoutFilter();
+
+            var parameter = Expression.Parameter(typeof(Workout));
+            var expression = filter.BuildExpression(parameter, type.ToString());
+
+            var compiledExpression = CompileExpression(parameter, expression);
+            Assert.False(compiledExpression(workout));
+        }
+
+        [Fact]
+        public void WorkoutTypeBuildExpression_ShouldThrow_WhenValueIsInvalid()
+        {
+            var filter = new TypeWorkoutFilter();
+            Assert.Throws<WrongFilterValueFormatException>(() =>
+                filter.BuildExpression(
+                    Expression.Parameter(typeof(Workout)),
+                    "abc"));
+        }
+
+        private Func<Workout, bool> CompileExpression(ParameterExpression parameter,Expression expression)
+        {
+            return Expression
+                    .Lambda<Func<Workout, bool>>(expression, parameter)
+                    .Compile();
+        }
+    }
+}
