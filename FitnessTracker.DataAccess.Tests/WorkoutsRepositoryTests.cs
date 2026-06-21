d3f734c69ff2253b37f005449e01b36628e6adef
Рома Авраменко <www.kioma220@gmail.com>
2026-06-21 17:16:51 +0300

    test: add photo tests

--- FitnessTracker.DataAccess.Tests/WorkoutsRepositoryTests.cs
+++ FitnessTracker.DataAccess.Tests/WorkoutsRepositoryTests.cs
@@ -267,6 +267,95 @@ namespace FitnessTracker.DataAccess.Tests
             await Assert.ThrowsAsync<EntityNotFoundException>(() => repository.AddExerciseAsync("2", exercise));
         }
 
+        [Fact]
+        public async Task AddPhotoAsync_ShouldAddPhoto_WhenWorkoutExists()
+        {
+            using var context = CreateDbContext();
+
+            var repository = new WorkoutsRepository(context, _orderingApplierMock.Object, _filterBuilderMock.Object);
+
+            var date = DateTime.UtcNow;
+            var workout = new Workout
+            {
+                Id = "1",
+                UserId = "user1",
+                Title = "Leg day",
+                Type = WorkoutType.Strength,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = date,
+                Exercises =
+                [
+                    new Exercise
+                    {
+                        Name = "Squat",
+                        Sets =
+                        [
+                            new Set(10, 100),
+                            new Set(8, 120)
+                        ]
+                    }
+                ],
+                ProgressPhotos = ["photo1.jpg"]
+            };
+
+            context.Workouts.Add(workout);
+            await context.SaveChangesAsync();
+
+            var photo = "photo2.jpg";
+
+            await repository.AddPhotoAsync("1", photo);
+
+            var dbWorkout = await context.Workouts
+                .FirstOrDefaultAsync(x => x.Id == "1");
+
+            Assert.NotNull(dbWorkout);
+            Assert.Equal(2, dbWorkout.ProgressPhotos.Count);
+            var photosdb = dbWorkout.ProgressPhotos.OrderBy(x => x).ToList();
+            Assert.Equal("photo1.jpg", photosdb[0]);
+            Assert.Equal("photo2.jpg", photosdb[1]);
+        }
+
+        [Fact]
+        public async Task AddPhotoAsync_ShouldThrow_WhenWorkoutNotExists()
+        {
+            using var context = CreateDbContext();
+
+            var repository = new WorkoutsRepository(context, _orderingApplierMock.Object, _filterBuilderMock.Object);
+
+            var date = DateTime.UtcNow;
+            var workout = new Workout
+            {
+                Id = "1",
+                UserId = "user1",
+                Title = "Leg day",
+                Type = WorkoutType.Strength,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = date,
+                Exercises =
+                [
+                    new Exercise
+                    {
+                        Name = "Squat",
+                        Sets =
+                        [
+                            new Set(10, 100),
+                            new Set(8, 120)
+                        ]
+                    }
+                ],
+                ProgressPhotos = ["photo1.jpg"]
+            };
+
+            context.Workouts.Add(workout);
+            await context.SaveChangesAsync();
+
+            var photo = "photo2.png";
+
+            await Assert.ThrowsAsync<EntityNotFoundException>(() => repository.AddPhotoAsync("2", photo));
+        }
+
 
         private FitnessTrackerDbContext CreateDbContext()
         {
