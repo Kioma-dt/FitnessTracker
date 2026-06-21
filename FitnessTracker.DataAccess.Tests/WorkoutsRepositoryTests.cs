319b063e3e49b0908a2d8907660f5a4a17a851c3
Рома Авраменко <www.kioma220@gmail.com>
2026-06-21 17:20:10 +0300

    test: delete wotkou test

--- FitnessTracker.DataAccess.Tests/WorkoutsRepositoryTests.cs
+++ FitnessTracker.DataAccess.Tests/WorkoutsRepositoryTests.cs
@@ -356,6 +356,111 @@ namespace FitnessTracker.DataAccess.Tests
             await Assert.ThrowsAsync<EntityNotFoundException>(() => repository.AddPhotoAsync("2", photo));
         }
 
+        [Fact]
+        public async Task DeleteAsync_ShouldDeleteWorkout_WhenWorkoutExists()
+        {
+            using var context = CreateDbContext();
+
+            var repository = new WorkoutsRepository(context, _orderingApplierMock.Object, _filterBuilderMock.Object);
+
+            var date = DateTime.UtcNow;
+            var workout1 = new Workout
+            {
+                Id = "1",
+                UserId = "user1",
+                Title = "Leg day",
+                Type = WorkoutType.Strength,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = date,
+                Exercises =
+                [
+                    new Exercise
+                    {
+                        Name = "Squat",
+                        Sets =
+                        [
+                            new Set(10, 100),
+                            new Set(8, 120)
+                        ]
+                    }
+                ],
+                ProgressPhotos = ["photo1.jpg"]
+            };
+            var workout2 = new Workout
+            {
+                Id = "2",
+                UserId = "user1",
+                Title = "Leg day",
+                Type = WorkoutType.Strength,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = date,
+                Exercises =
+                [
+                    new Exercise
+                    {
+                        Name = "Squat",
+                        Sets =
+                        [
+                            new Set(10, 100),
+                            new Set(8, 120)
+                        ]
+                    }
+                ],
+                ProgressPhotos = ["photo1.jpg"]
+            };
+
+            context.Workouts.Add(workout1);
+            context.Workouts.Add(workout2);
+            await context.SaveChangesAsync();
+
+            await repository.DeleteAsync("1");
+
+            var dbWorkout = await context.Workouts
+                .FirstOrDefaultAsync(x => x.Id == "1");
+
+            Assert.Null(dbWorkout);
+            Assert.Equal(1, context.Workouts.Count());
+        }
+
+        [Fact]
+        public async Task DeleteAsync_ShouldThrow_WhenWorkoutNotExists()
+        {
+            using var context = CreateDbContext();
+
+            var repository = new WorkoutsRepository(context, _orderingApplierMock.Object, _filterBuilderMock.Object);
+
+            var date = DateTime.UtcNow;
+            var workout = new Workout
+            {
+                Id = "1",
+                UserId = "user1",
+                Title = "Leg day",
+                Type = WorkoutType.Strength,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = date,
+                Exercises =
+                [
+                    new Exercise
+                    {
+                        Name = "Squat",
+                        Sets =
+                        [
+                            new Set(10, 100),
+                            new Set(8, 120)
+                        ]
+                    }
+                ],
+                ProgressPhotos = ["photo1.jpg"]
+            };
+
+            context.Workouts.Add(workout);
+            await context.SaveChangesAsync();
+
+            await Assert.ThrowsAsync<EntityNotFoundException>(() => repository.DeleteAsync("2"));
+        }
 
         private FitnessTrackerDbContext CreateDbContext()
         {
