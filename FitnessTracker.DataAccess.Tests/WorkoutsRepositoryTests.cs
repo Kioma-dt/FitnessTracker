f9567fec740088ced1f0f701f598efeb1d1e83d5
Рома Авраменко <www.kioma220@gmail.com>
2026-06-21 18:10:10 +0300

    test: get total workout count tests

--- FitnessTracker.DataAccess.Tests/WorkoutsRepositoryTests.cs
+++ FitnessTracker.DataAccess.Tests/WorkoutsRepositoryTests.cs
@@ -998,6 +998,194 @@ namespace FitnessTracker.DataAccess.Tests
             Assert.Equal("photo1.jpg", workoutUpdated.ProgressPhotos[0]);
         }
 
+        [Fact]
+        public async Task GetTotalCountByUserAsync_ShouldReturnCount_WhenFilterIsNull()
+        {
+            using var context = CreateDbContext();
+
+            var filterBuilderMock = new Mock<IWorkoutFilterExpressionBuilder>();
+
+            var repository = new WorkoutsRepository(context, _orderingApplierMock.Object, filterBuilderMock.Object);
+
+            var workout1 = new Workout
+            {
+                Id = "1",
+                UserId = "user1",
+                Title = "Leg day",
+                Type = WorkoutType.Strength,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = new DateTime(2026, 6, 1).ToUniversalTime(),
+                Exercises =[],
+                ProgressPhotos = []
+            };
+
+            var workout2 = new Workout
+            {
+                Id = "2",
+                UserId = "user1",
+                Title = "Leg day",
+                Type = WorkoutType.Strength,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = new DateTime(2026, 6, 1).ToUniversalTime(),
+                Exercises = [],
+                ProgressPhotos = []
+            };
+
+            var workout3 = new Workout
+            {
+                Id = "3",
+                UserId = "user2",
+                Title = "Leg day",
+                Type = WorkoutType.Strength,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = new DateTime(2026, 6, 1).ToUniversalTime(),
+                Exercises = [],
+                ProgressPhotos = []
+            };
+
+            context.Workouts.AddRange(
+                workout1,
+                workout2,
+                workout3
+            );
+            await context.SaveChangesAsync();
+
+            var count = await repository.GetTotalCountByUserAsync("user1");
+
+            Assert.Equal(2, count);
+            filterBuilderMock.Verify(x => x.BuildFilterExpression(
+                It.IsAny<IEnumerable<WorkoutFilterDTO>>()),
+                Times.Never);
+        }
+
+        [Fact]
+        public async Task GetTotalCountByUserAsync_ShouldReturnCount_WhenFilterIsNotNull()
+        {
+            using var context = CreateDbContext();
+
+            var filterBuilderMock = new Mock<IWorkoutFilterExpressionBuilder>();
+            filterBuilderMock.Setup(x => x.BuildFilterExpression(It.IsAny<IEnumerable<WorkoutFilterDTO>>()))
+                        .Returns(workout => workout.Type == WorkoutType.Strength);
+
+            var repository = new WorkoutsRepository(context, _orderingApplierMock.Object, filterBuilderMock.Object);
+
+            var workout1 = new Workout
+            {
+                Id = "1",
+                UserId = "user1",
+                Title = "Leg day",
+                Type = WorkoutType.Strength,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = new DateTime(2026, 6, 1).ToUniversalTime(),
+                Exercises = [],
+                ProgressPhotos = []
+            };
+
+            var workout2 = new Workout
+            {
+                Id = "2",
+                UserId = "user1",
+                Title = "Leg day",
+                Type = WorkoutType.Cardio,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = new DateTime(2026, 6, 1).ToUniversalTime(),
+                Exercises = [],
+                ProgressPhotos = []
+            };
+
+            var workout3 = new Workout
+            {
+                Id = "3",
+                UserId = "user2",
+                Title = "Leg day",
+                Type = WorkoutType.Strength,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = new DateTime(2026, 6, 1).ToUniversalTime(),
+                Exercises = [],
+                ProgressPhotos = []
+            };
+
+            context.Workouts.AddRange(
+                workout1,
+                workout2,
+                workout3
+            );
+            await context.SaveChangesAsync();
+
+            var count = await repository.GetTotalCountByUserAsync("user1", new List<WorkoutFilterDTO>());
+
+            Assert.Equal(1, count);
+            filterBuilderMock.Verify(x => x.BuildFilterExpression(
+                It.IsAny<IEnumerable<WorkoutFilterDTO>>()),
+                Times.Once);
+        }
+
+        [Fact]
+        public async Task GetTotalCountByUserAsync_ShouldZero_WhenUserHasNoWorkouts()
+        {
+            using var context = CreateDbContext();
+
+            var filterBuilderMock = new Mock<IWorkoutFilterExpressionBuilder>();
+
+            var repository = new WorkoutsRepository(context, _orderingApplierMock.Object, filterBuilderMock.Object);
+
+            var workout1 = new Workout
+            {
+                Id = "1",
+                UserId = "user1",
+                Title = "Leg day",
+                Type = WorkoutType.Strength,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = new DateTime(2026, 6, 1).ToUniversalTime(),
+                Exercises = [],
+                ProgressPhotos = []
+            };
+
+            var workout2 = new Workout
+            {
+                Id = "2",
+                UserId = "user1",
+                Title = "Leg day",
+                Type = WorkoutType.Cardio,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = new DateTime(2026, 6, 1).ToUniversalTime(),
+                Exercises = [],
+                ProgressPhotos = []
+            };
+
+            var workout3 = new Workout
+            {
+                Id = "3",
+                UserId = "user2",
+                Title = "Leg day",
+                Type = WorkoutType.Strength,
+                Duration = TimeSpan.FromMinutes(60),
+                CaloriesBurned = 500,
+                WorkoutDate = new DateTime(2026, 6, 1).ToUniversalTime(),
+                Exercises = [],
+                ProgressPhotos = []
+            };
+
+            context.Workouts.AddRange(
+                workout1,
+                workout2,
+                workout3
+            );
+            await context.SaveChangesAsync();
+
+            var count = await repository.GetTotalCountByUserAsync("user4");
+
+            Assert.Equal(0, count);
+        }
+
         private FitnessTrackerDbContext CreateDbContext()
         {
             var options = new DbContextOptionsBuilder<FitnessTrackerDbContext>()
