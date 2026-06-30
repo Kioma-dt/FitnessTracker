9613a51fa30bb404108f4f69aceac6afb408df5d
Рома Авраменко <www.kioma220@gmail.com>
2026-06-30 13:59:47 +0300

    feat: add medaitr for application

--- FitnessTracker.API/Program.cs
+++ FitnessTracker.API/Program.cs
@@ -176,6 +176,7 @@ namespace FitnessTracker.API
                 });
 
             builder.Services
+                .AddApplication()
                 .AddInfrastructure()
                 .AddMappers()
                 .AddRepositories()
