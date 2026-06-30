7b76a5e9a2679417663cd962667e6c73b6cdc0ce
Рома Авраменко <www.kioma220@gmail.com>
2026-06-30 19:22:53 +0300

    fix: extract all complex dependecy injections from program file

---
+++ FitnessTracker.API/DependencyInjectionExtensions/FitnessTrackerDbContextConfigured.cs
@@ -0,0 +1,40 @@
+﻿using FitnessTracker.DataAccess;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace FitnessTracker.API.DependencyInjectionExtensions
+{
+    public static class FitnessTrackerDbContextConfigured
+    {
+        public static IServiceCollection AddFitnessTrackerDbContextConfigured(this IServiceCollection services)
+        {
+            var host = Environment.GetEnvironmentVariable("DB_HOST");
+            var port = Environment.GetEnvironmentVariable("DB_PORT");
+            var db = Environment.GetEnvironmentVariable("DB_NAME");
+            var user = Environment.GetEnvironmentVariable("DB_USER");
+            var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+            if (host is null ||
+                port is null ||
+                db is null ||
+                user is null ||
+                password is null)
+            {
+                throw new EnviormnetVariableNotFoundException("Can't Find Enviorment Variables for Connection String");
+            }
+
+            var connectionString =
+                    $"Host={host};Port={port};Database={db};Username={user};Password={password}";
+
+            services.AddDbContext<FitnessTrackerDbContext>(options =>
+                options.UseNpgsql(connectionString,
+                o =>
+                {
+                    o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
+                    o.EnableRetryOnFailure(3, TimeSpan.FromSeconds(30), null);
+                }));
+
+            return services;
+        }
+    }
+}
--- FitnessTracker.API/Program.cs
+++ FitnessTracker.API/Program.cs
@@ -30,133 +30,16 @@ namespace FitnessTracker.API
 
             builder.Services.AddSwaggerGenConfugured();
 
-            builder.Services.AddAuthorization(options =>
-            {
-                options.AddPolicy(
-                    "WorkoutOwner",
-                    policy =>
-                    {
-                        policy.Requirements.Add(
-                            new WorkoutOwnerRequirement());
-                    });
-            });
+            builder.Services.AddAuthorizationConfigured();
 
-            var authOptions = builder.Configuration.GetSection("Authentication");
-            var jwtKey = builder.Configuration.GetValue<string>("JWT_KEY");
-
-            if (authOptions is null || jwtKey is null)
-            {
-                throw new EnviormnetVariableNotFoundException("No enviormnet variables for authorization");
-            }
-            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
-
-            builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-               .AddJwtBearer(options =>
-               {
-                   options.TokenValidationParameters = new TokenValidationParameters
-                   {
-                       ValidateIssuer = true,
-                       ValidIssuer = authOptions["Issuer"],
-                       ValidateAudience = true,
-                       ValidAudience = authOptions["Audience"],
-                       ValidateLifetime = true,
-                       IssuerSigningKey = key,
-                       ValidateIssuerSigningKey = true
-                   };
-                   options.Events = new JwtBearerEvents
-                   {
-                       OnTokenValidated = context =>
-                       {
-                           var userId = context.Principal?
-                               .FindFirst(ClaimTypes.NameIdentifier)
-                               ?.Value;
-
-                           if (string.IsNullOrEmpty(userId))
-                           {
-                               context.Fail("JWT token does not contain user id");
-                           }
-
-                           return Task.CompletedTask;
-                       },
-                       OnChallenge = context =>
-                       {
-                           context.HandleResponse();
-
-                           context.Response.StatusCode = 401;
-                           context.Response.ContentType = "application/json";
-
-                           return context.Response.WriteAsJsonAsync(new ProblemDetails
-                           {
-                               Status = 401,
-                               Title = "Unauthorized",
-                               Detail = "Token is missing or invalid"
-                           });
-                       },
-
-                       OnForbidden = context =>
-                       {
-                           context.Response.StatusCode = 403;
-                           context.Response.ContentType = "application/json";
-
-                           return context.Response.WriteAsJsonAsync(new ProblemDetails
-                           {
-                               Status = 403,
-                               Title = "Forbiden",
-                               Detail = "You don't have access to this resource"
-                           });
-                       }
-                   };
-
-               });
+            builder.Services.AddAuthenticationConfigured(builder.Configuration);
 
             builder.Services.AddProblemDetails();
             builder.Services.AddExceptionHandler<GlobalExceptionHandler>();
 
-            var host = Environment.GetEnvironmentVariable("DB_HOST");
-            var port = Environment.GetEnvironmentVariable("DB_PORT");
-            var db = Environment.GetEnvironmentVariable("DB_NAME");
-            var user = Environment.GetEnvironmentVariable("DB_USER");
-            var password = Environment.GetEnvironmentVariable("DB_PASSWORD");
+            builder.Services.AddFitnessTrackerDbContextConfigured();
 
-            if (host is null ||
-                port is null ||
-                db is null ||
-                user is null ||
-                password is null)
-            {
-                throw new EnviormnetVariableNotFoundException("Can't Find Enviorment Variables for Connection String");
-            }
-
-            var connectionString =
-                    $"Host={host};Port={port};Database={db};Username={user};Password={password}";
-
-            builder.Services.AddDbContext<FitnessTrackerDbContext>(options =>
-                options.UseNpgsql(connectionString,
-                o =>
-                {
-                    o.UseQuerySplittingBehavior(QuerySplittingBehavior.SplitQuery);
-                    o.EnableRetryOnFailure(3, TimeSpan.FromSeconds(30), null);
-                }));
-
-            builder.Services.AddControllers()
-                .ConfigureApiBehaviorOptions(options =>
-                {
-                    options.InvalidModelStateResponseFactory = context =>
-                    {
-                        var errors = context.ModelState
-                        .Where(x => x.Value?.Errors.Count > 0)
-                        .SelectMany(x => x.Value!.Errors.Select(x => x.ErrorMessage))
-                        .ToList();
-
-                        return new BadRequestObjectResult(new ProblemDetails
-                        {
-                            Status = 400,
-                            Title = "Validation Failed",
-                            Detail = "Errors while validating request",
-                            Extensions = { ["errors"] = errors }
-                        });
-                    };
-                });
+            builder.Services.AddControllersConfigured();
 
             builder.Services
                 .AddApplication()
